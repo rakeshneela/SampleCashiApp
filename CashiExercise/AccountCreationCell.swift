74cd55505a3703fc54eeef8dd17c57cccb476a32
Rakesh Neela <rakeshneela@outlook.com>
2020-01-04 16:42:40 -0800

    Sample Cashi App

---
+++ CashiExercise/AccountCreationCell.swift
@@ -0,0 +1,24 @@
+//
+//  AccountCreationCell.swift
+//  CashiExercise
+//
+//  Created by Rakesh Neela on 10/21/19.
+//  Copyright © 2019 Rakesh Neela. All rights reserved.
+//
+
+import UIKit
+
+class AccountCreationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
